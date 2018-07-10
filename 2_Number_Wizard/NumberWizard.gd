c857d2059d4b5ac2170c427d2e9a1a78deda56f5
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 17:11:44 +0100

    2_Number_Wizard: Code Cleanup
    - Code cleaned up a bit, to improve readability
    - Game now restarts once a correct guess has been made

--- 2_Number_Wizard/NumberWizard.gd
+++ 2_Number_Wizard/NumberWizard.gd
@@ -1,11 +1,19 @@
 extends Node
 
 # min and max are function names
-var minVal = 1
-var maxVal = 1000
-var guess = 500
+var minVal
+var maxVal
+var guess
 
 func _ready():
+	start_game()
+
+
+func start_game():
+	minVal = 1
+	maxVal = 1000
+	guess = 500
+
 	print("Welcome to Number Wizard")
 	print("Pick a number between ", minVal, " and ", maxVal, ", don't tell me what it is ...")
 	print("Tell me if your number is higher or lower than ", guess, "?")
@@ -16,11 +24,15 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		minVal = guess
-		guess = (minVal + maxVal) / 2
-		print("Is it higher or lower than ", guess, "?")
+		next_guess()
 	elif Input.is_action_just_pressed("ui_down"):
 		maxVal = guess
-		guess = (minVal + maxVal) / 2
-		print("Is it higher or lower than ", guess, "?")
+		next_guess()
 	elif Input.is_action_just_pressed("ui_accept"):
 		print("I guessed correct :)")
+		start_game()
+
+
+func next_guess():
+	guess = (minVal + maxVal) / 2
+	print("Is it higher or lower than ", guess, "?")
