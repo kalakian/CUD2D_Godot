134ab57a33f904d076b062bf231878de00163302
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 16:50:19 +0100

    2_Number_Wizard: User Input
    - Player is prompted for user input
    - For each of the valid keys, a message is output to the console

--- 2_Number_Wizard/NumberWizard.gd
+++ 2_Number_Wizard/NumberWizard.gd
@@ -8,7 +8,14 @@ func _ready():
 	
 	print("Welcome to Number Wizard")
 	print("Pick a number between ", minVal, " and ", maxVal, ", don't tell me what it is ...")
+	print("Tell me if your number is higher or lower than 500?")
+	print("Push Up = Higher, Push Down = Lower, Push Enter = Correct")
 
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_up"):
+		print("Up arrow key was pressed.")
+	if Input.is_action_just_pressed("ui_down"):
+		print("Down arrow key was pressed.")
+	if Input.is_action_just_pressed("ui_accept"):
+		print("Return key was pressed.")
