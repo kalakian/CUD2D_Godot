6d6a6d31162ec476bde70352d7bb7a16aa07fb86
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 17:08:19 +0100

    2_Number_Wizard: Calculate Guess
    - Guess is now calculated based on higher or lower input from player

--- 2_Number_Wizard/NumberWizard.gd
+++ 2_Number_Wizard/NumberWizard.gd
@@ -6,21 +6,21 @@ var maxVal = 1000
 var guess = 500
 
 func _ready():
-	
 	print("Welcome to Number Wizard")
 	print("Pick a number between ", minVal, " and ", maxVal, ", don't tell me what it is ...")
 	print("Tell me if your number is higher or lower than ", guess, "?")
-	print("Push Up = Higher, Push Down = Lower, Push Enter = Correct")
+	
+	maxVal += 1 # this ensures the max value can be included in guesses
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
-		print("Up arrow key was pressed.")
 		minVal = guess
-		print(guess)
+		guess = (minVal + maxVal) / 2
+		print("Is it higher or lower than ", guess, "?")
 	elif Input.is_action_just_pressed("ui_down"):
-		print("Down arrow key was pressed.")
 		maxVal = guess
-		print(guess)
+		guess = (minVal + maxVal) / 2
+		print("Is it higher or lower than ", guess, "?")
 	elif Input.is_action_just_pressed("ui_accept"):
-		print("Return key was pressed.")
+		print("I guessed correct :)")
