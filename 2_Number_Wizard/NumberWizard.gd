ad65a3a4dda1f403d384bc7faa361b54e11fefa5
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 16:44:46 +0100

    2_Number_Wizard: Variables
    - Min and Max of the intro text now declared as variables

--- 2_Number_Wizard/NumberWizard.gd
+++ 2_Number_Wizard/NumberWizard.gd
@@ -2,8 +2,12 @@ extends Node
 
 
 func _ready():
+	# min and max are function names
+	var minVal = 1
+	var maxVal = 1000
+	
 	print("Welcome to Number Wizard")
-	print("Pick a number between 1 and 1000, don't tell me what it is ...")
+	print("Pick a number between ", minVal, " and ", maxVal, ", don't tell me what it is ...")
 
 
 func _process(delta):
