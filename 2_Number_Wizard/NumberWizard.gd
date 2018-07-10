728b96d7decd6c7c67f116ed437ef0800b2d4208
Derek Stobbs <dstobbs@googlemail.com>
2018-07-10 17:03:07 +0100

    2_Number_Wizard: Adjust Min or Max
    - Min or Max adjusted depending on whether Player chooses higher or lower

--- 2_Number_Wizard/NumberWizard.gd
+++ 2_Number_Wizard/NumberWizard.gd
@@ -1,21 +1,26 @@
 extends Node
 
+# min and max are function names
+var minVal = 1
+var maxVal = 1000
+var guess = 500
 
 func _ready():
-	# min and max are function names
-	var minVal = 1
-	var maxVal = 1000
 	
 	print("Welcome to Number Wizard")
 	print("Pick a number between ", minVal, " and ", maxVal, ", don't tell me what it is ...")
-	print("Tell me if your number is higher or lower than 500?")
+	print("Tell me if your number is higher or lower than ", guess, "?")
 	print("Push Up = Higher, Push Down = Lower, Push Enter = Correct")
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		print("Up arrow key was pressed.")
+		minVal = guess
+		print(guess)
 	elif Input.is_action_just_pressed("ui_down"):
 		print("Down arrow key was pressed.")
+		maxVal = guess
+		print(guess)
 	elif Input.is_action_just_pressed("ui_accept"):
 		print("Return key was pressed.")
